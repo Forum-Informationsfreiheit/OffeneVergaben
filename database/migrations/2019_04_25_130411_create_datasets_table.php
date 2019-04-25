9d3443f35a23a00c8c51f1eec620cbdcdab658d2
ives_markus <markus.wallisch@interactives.eu>
2019-04-25 16:33:40 +0200

    Add migrations for base tables.

---
+++ database/migrations/2019_04_25_130411_create_datasets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDatasetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datasets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('origin_id');
+            $table->string('url',500);
+            $table->unsignedInteger('version_scraped');
+            $table->unsignedInteger('version');             // processed version
+            $table->timestamp('last_modified_at')->nullable();
+            $table->timestamp('last_scraped_at')->nullable();
+            $table->timestamp('last_processed_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('origin_id')->references('id')->on('origins')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datasets');
+    }
+}
