5d37c79fbec2a96c1c16302bcf44b05db083495e
ives_markus <markus.wallisch@interactives.eu>
2019-09-22 11:07:31 +0200

    Small fix for non existing routes.

--- resources/views/public/partials/topbar.blade.php
+++ resources/views/public/partials/topbar.blade.php
@@ -18,13 +18,13 @@
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->route()->getName() == 'public::auftraggeber' ? 'active' : '' }}" href="{{ route('public::auftraggeber') }}">{{ __('Auftraggeber') }}</a>
+                    <a class="nav-link {{ request()->route() && request()->route()->getName() == 'public::auftraggeber' ? 'active' : '' }}" href="{{ route('public::auftraggeber') }}">{{ __('Auftraggeber') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->route()->getName() == 'public::lieferanten' ? 'active' : '' }}" href="{{ route('public::lieferanten') }}">{{ __('Lieferanten') }}</a>
+                    <a class="nav-link {{ request()->route() && request()->route()->getName() == 'public::lieferanten' ? 'active' : '' }}" href="{{ route('public::lieferanten') }}">{{ __('Lieferanten') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->route()->getName() == 'public::auftraege' ? 'active' : '' }}" href="{{ route('public::auftraege') }}">{{ __('Aufträge') }}</a>
+                    <a class="nav-link {{ request()->route() && request()->route()->getName() == 'public::auftraege' ? 'active' : '' }}" href="{{ route('public::auftraege') }}">{{ __('Aufträge') }}</a>
                 </li>
             </ul>
         </div>
