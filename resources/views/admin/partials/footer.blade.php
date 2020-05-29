b5f483fdc9b31377c642229da80b7d8cc6578bb2
ives_markus <markus.wallisch@interactives.eu>
2020-05-29 10:48:55 +0200

    Modify readme. Change copyright text in admin footer.

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Ives Lara v5.8 Admin 2019</span>
+            <span>Copyright &copy; Ives Lara v5.8 Admin {{ \Carbon\Carbon::now()->year }}</span>
         </div>
     </div>
 </footer>
